6e236be7f716c5d7e2d2b0e80023532951b028b5
QuicklyDG <quicklydeguzman@gmail.com>
2025-01-18 00:37:58 +0800

    added footer

---
+++ resources/js/app/pages/landing_page/sections/footer-section.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function FooterSection() {
+    return (
+        <div>
+            <div className='h-10 flex items-center justify-center bg-blue-900 text-white'>© 2025 Parasat Cable TV, Inc. All rights reserved.</div>
+        </div>
+    )
+}
--- resources/js/app/pages/landing_page/sections/plan-list-section.jsx
+++ resources/js/app/pages/landing_page/sections/plan-list-section.jsx
@@ -7,7 +7,7 @@ export default function PlanListSection() {
 
     return (
         <div>
-            <div className="bg-sky-600 pb-5">
+            <div className="bg-sky-600 pb-10">
                 <div className="mx-auto max-w-7xl px-6 lg:px-8">
                     <div className="flex flex-col gap-3">
                         <p className='mt-4 text-balance text-5xl font-semibold tracking-tight text-white sm:text-5xl mb-4 shadow-blue-300 shadow-lg w-full'>
@@ -34,7 +34,7 @@ export default function PlanListSection() {
                                 href="http://127.0.0.1:8000/logins"
                             />
                         </div>
-                        <div className='className="isolate -mt-16 grid max-w-sm grid-cols-1 gap-y-16 gap-x-2 divide-y divide-gray-100 sm:mx-auto lg:-mx-8 lg:mt-0 lg:max-w-none lg:grid-cols-3 lg:divide-x lg:divide-y-0 xl:-mx-4"'>
+                        <div className='className="isolate -mt-16 grid max-w-sm grid-cols-1 gap-y-16 gap-x-3 divide-y divide-gray-100 sm:mx-auto lg:-mx-8 lg:mt-0 lg:max-w-none lg:grid-cols-3 lg:divide-x lg:divide-y-0 xl:-mx-4"'>
                             <OfferPlanCardComponent
                                 name="Up to 40Mbps"
                                 monthly="₱999"
