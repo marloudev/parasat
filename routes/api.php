2dbc41c358dfb4b466d1d0b75b1f37634de84aba
QuicklyDG <quicklydeguzman@gmail.com>
2025-02-23 19:29:47 +0800

    fix dashboard

--- routes/api.php
+++ routes/api.php
@@ -20,7 +20,7 @@ Route::get('/user', function (Request $request) {
 Route::resource('application', ApplicationController::class);
 Route::resource('account', AccountController::class);
 Route::resource('internet_plan', InternetPlansController::class);
-Route::resource('dashboard', DashboardController::class);
+Route::get('/dashboard', [DashboardController::class, 'index']);
 Route::resource('send_email', EmailController::class);
 
 Route::middleware('auth:sanctum')->prefix('/')->group(function () {
