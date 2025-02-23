ff0f24409306f71da4f8eddfd94c013830a2a904
QuicklyDG <quicklydeguzman@gmail.com>
2025-02-23 14:28:22 +0800

    adjust total font

---
+++ public/build/assets/page-Bq3amqp5.js
@@ -0,0 +1,1 @@
+import{j as e}from"./app-DHuoODYV.js";function t(){return e.jsx("div",{children:"CSRPage"})}export{t as default};
--- resources/js/app/pages/admin/dashboard/components/dashboard-card-components.jsx
+++ resources/js/app/pages/admin/dashboard/components/dashboard-card-components.jsx
@@ -5,7 +5,7 @@ export default function DashboardCardComponents({ name, icon, total, bgColor }) 
         <div className={`divide-y w-full divide-gray-200 overflow-hidden rounded-2xl shadow-xl ${bgColor}`}>
             <div className="px-4 py-5 sm:p-6 h-80 text-center">
                 <div className='text-[8rem] sm:text-[11rem] text-white'>
-                    {total}
+                    <b>{total}</b>
                 </div>
             </div>
             <div className="px-4 py-4 sm:px-6 text-center bg-white">
