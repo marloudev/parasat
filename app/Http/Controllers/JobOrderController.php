4d4d93295db0d899c151aa2a4dd8e8356dd07ed1
Marlou Dev <marlou.dev.app@gmail.com>
2025-04-24 19:15:08 +0800

    another changes

--- app/Http/Controllers/JobOrderController.php
+++ app/Http/Controllers/JobOrderController.php
@@ -72,7 +72,7 @@ class JobOrderController extends Controller
         $app = Application::where('id', $plan->application_id)->first();
         if ($app) {
             if ($request->status == 'Approved Installation') {
-                Mail::to($request->email)->send(new ApprovedApplication($app));
+                Mail::to($app->email)->send(new ApprovedApplication($app));
                 $app->update([
                     'status' => 'Approved'
                 ]);
